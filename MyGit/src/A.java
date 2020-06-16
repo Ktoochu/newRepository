3368b33107b3c6731be63fa323c77075dab155a2
alswl <alswl@LAPTOP-H9OHTV61>
2020-06-16 20:50:10 +0900

    devB

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -4,6 +4,9 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First");
+
+		System.out.println("devB");
+		new B();
 	}
 
 }
--- MyGit/src/B.java
+++ MyGit/src/B.java
@@ -2,5 +2,6 @@
 public class B {
 	public B() {
 		System.out.println("This is B()");
+		System.out.println("Change in devB branch");
 	}
 }
