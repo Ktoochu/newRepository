5a7ef3621222a4ad9e2bc8ea184468ae3aa99f5f
alswl <alswl@LAPTOP-H9OHTV61>
2020-06-16 20:51:43 +0900

    devC

--- MyGit/src/A.java
+++ MyGit/src/A.java
@@ -4,6 +4,9 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First");
+		
+		System.out.println("devC");
+		new C();
 	}
 
 }
